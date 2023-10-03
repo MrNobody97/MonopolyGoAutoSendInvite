d3a659e022684a788002f14b50104008cfd76818
MrNobody97 <leo.sanacore@gmail.com>
2023-10-03 20:11:46 +0200

    Update Monopoly Go Invite.bat

--- Monopoly Go Invite.bat
+++ Monopoly Go Invite.bat
@@ -8,19 +8,19 @@ set loopvar=1
 cls
 :start
 @echo off
-if %loopvar% gtr %times% (goto :done) else (set /a loopvar=%loopvar%+1 && echo %loopvar% Times, Start && goto :repeat)
+if %loopvar% gtr %times% (goto :done) else (set /a loopvar=%loopvar%+1 && echo %loopvar% Times, Start && goto :repeat) // Its for loop
 :repeat
-FOR /F "tokens=1,2" %%a IN ('adb devices') DO (
-    IF "%%b" == "device" ( adb -s %%a shell pm clear -r com.google.android.gms )
-    IF "%%b" == "device" ( adb -s %%a shell pm clear -r com.scopely.monopolygo )
-    IF "%%b" == "device" ( adb -s %%a shell am start -a android.intent.action.VIEW -d %link% )
+FOR /F "tokens=1,2" %%a IN ('adb devices') DO (  // Code for send shell command to all adb devices
+    IF "%%b" == "device" ( adb -s %%a shell pm clear -r com.google.android.gms ) // Clean Play Service Cache
+    IF "%%b" == "device" ( adb -s %%a shell pm clear -r com.scopely.monopolygo ) // Clean Monopoly cache
+    IF "%%b" == "device" ( adb -s %%a shell am start -a android.intent.action.VIEW -d %link% ) // Open Browser with link
 )
-timeout 60
-cls
+timeout 60  // Can modify, timer for completely loading game
+cls // Clean command on windows
 FOR /F "tokens=1,2" %%a IN ('adb devices') DO (
-    IF "%%b" == "device" ( adb -s %%a shell am force-stop com.android.browser )
+    IF "%%b" == "device" ( adb -s %%a shell am force-stop com.android.browser )  // Force closing browser
 )
-goto start
+goto start  
 
 :done
 echo Finish
