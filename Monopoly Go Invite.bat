3f50087493d81b702cc21efbfa3c4eddc155826f
MrNobody97 <leo.sanacore@gmail.com>
2023-10-08 18:21:00 +0200

    Add files via upload

--- Monopoly Go Invite.bat
+++ Monopoly Go Invite.bat
@@ -1,24 +1,25 @@
 @echo off
-:inizio
-echo Insert Link:
-set /p link=
+:init
+echo Insert Only Code:
+set /p code=
 echo How many times do you have to send the invitation?:
 set /p times=
 set loopvar=1
+
 cls
 :start
 @echo off
-if %loopvar% gtr %times% (goto :done) else (set /a loopvar=%loopvar%+1 && echo %loopvar% Times, Start && goto :repeat)
+if %loopvar% gtr %times% (goto :done) else (set /a loopvar=%loopvar%+1 && echo %loopvar% Times, Start && goto :repeat) 
 :repeat
 FOR /F "tokens=1,2" %%a IN ('adb devices') DO (  
     IF "%%b" == "device" ( adb -s %%a shell pm clear -r com.google.android.gms ) 
     IF "%%b" == "device" ( adb -s %%a shell pm clear -r com.scopely.monopolygo ) 
-    IF "%%b" == "device" ( adb -s %%a shell am start -a android.intent.action.VIEW -d %link% ) 
+    IF "%%b" == "device" ( adb -s %%a shell am start -a android.intent.action.VIEW -d https://s.scope.ly/%code% )
 )
-timeout 60 
+timeout /t 60                                                                                           
 cls 
 FOR /F "tokens=1,2" %%a IN ('adb devices') DO (
-    IF "%%b" == "device" ( adb -s %%a shell am force-stop com.android.browser )  
+    IF "%%b" == "device" ( adb -s %%a shell am force-stop com.android.browser ) 
 )
 goto start  
 
@@ -26,4 +27,4 @@ goto start
 echo Finish
 pause
 cls
-goto inizio
+goto init
